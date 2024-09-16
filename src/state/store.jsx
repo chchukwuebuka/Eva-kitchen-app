6f39053f5a482b23aaae87608635ee043c0e244c
chukwuebuka <emmanuel.henry71@gmail.com>
2024-09-16 15:51:57 +0100

    feat:: added shipping page

--- src/state/store.jsx
+++ src/state/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counter/counterSlice"; 
+import paymentReducer from "./payment";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    payment: paymentReducer
   },
 });
